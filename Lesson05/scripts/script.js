41c5d4873113c3c8eb795a5e844a1b3a47471e93
cnwelch23 <car15026@byui.edu>
2021-05-24 22:28:25 -0500

    commit preston page

---
+++ Lesson05/scripts/script.js
@@ -0,0 +1,11 @@
+var d = new Date();
+var n = d.getDay();
+console.log(n);
+
+if (n == 1) {
+    document.getElementById("banner-friday").innerHTML =
+    "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+}
+
+//for date how to format var d and same with banner 
+var 
